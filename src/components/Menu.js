dc936f39973d042869c67a87b62431926fed7c1f
Shivam Mahajan <spmshivam@gmail.com>
2023-05-23 00:46:41 -0400

    Created basic single page

---
+++ src/components/Menu.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Menu = () => {
+
+    const posts = [
+        {
+            id: 1,
+            title: "Lorem ipsum",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            img:"https://picsum.photos/200/300",
+        },
+        {
+            id: 2,
+            title: "Lorem ipsum",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            img:"https://picsum.photos/200/300",
+        },
+        {
+            id: 3,
+            title: "Lorem ipsum",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            img:"https://picsum.photos/200/300",
+        },
+
+        {
+            id: 4,
+            title: "Lorem ipsum",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            img:"https://picsum.photos/200/300",
+        }
+    ]
+
+  return (
+    <div className='menu'>
+        <h1>Other posts you may like</h1>
+        {posts.map((post) => (
+            <div class="post" key={post.id}>
+                <img src={post.img} alt=""/>
+                <h2>{post.title}</h2>
+                <button>Read More</button>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default Menu
--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import { Link } from 'react-router-dom'
+import Menu from '../components/Menu'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div class="content">
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <div class="user">
+          <img src="https://picsum.photos/200/300" alt=""/>
+          <div class="info">
+            <span>Shivam</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div class="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt=""/>
+            </Link>
+            <img src={Delete} alt=""/>
+          </div>
+        </div>
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel et ratione beatae! Cum animi qui architecto sint placeat molestias ex, omnis id porro possimus deserunt adipisci perspiciatis odio voluptas. Aliquid?</p>
+      
+      </div>
+      
+      <Menu/>
+    </div>
   )
 }
 
