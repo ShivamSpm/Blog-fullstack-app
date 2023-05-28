c932046d6257bd65067532e80e9616bf2af82d7a
Shivam Mahajan <spmshivam@gmail.com>
2023-05-28 14:51:30 -0400

    Added the login functionality using JWT tokens, localstorage and React AuthContext

--- api/index.js
+++ api/index.js
@@ -3,12 +3,17 @@ import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import cors from 'cors'
+import cookieParser from "cookie-parser";
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000', // Replace with your client's origin
+    credentials: true
+  }))
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from '../img/logo.png'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/authContext'
 
 const Navbar = () => {
+
+  const {currentUser, logout} = useContext(AuthContext);
+
   return (
     <div className='navbar'>
       <div className='container'>
@@ -16,8 +20,14 @@ const Navbar = () => {
           <Link className='link' to='/?cat=cinema'><h6>CINEMA</h6></Link>
           <Link className='link' to='/?cat=design'><h6>DESIGN</h6></Link>
           <Link className='link' to='/?cat=food'><h6>FOOD</h6></Link>
-          <span>Shivam</span>
-          <span>Logout</span>
+          <span>{currentUser?.username}</span>
+          {currentUser? (
+            <span onClick={logout}>Logout</span>
+          ):(
+            <Link className='link' to="/login">
+              Login
+            </Link>
+          )}
           <span className='write'>
             <Link className='link'to='/write'>Write</Link>
           </span>
