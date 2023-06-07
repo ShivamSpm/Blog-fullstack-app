5f5dba18250252ab413aa740f2a4b507f21481bb
Shivam Mahajan <spmshivam@gmail.com>
2023-06-06 20:08:33 -0400

    Replaced the config file with .env

--- api/config_example.js
+++
@@ -1,4 +0,0 @@
-const databasePass = "<Enter_Password_here>";
-export default databasePass;
-
-// Change the name of this file to config.js
--- api/db.js
+++ api/db.js
@@ -4,6 +4,6 @@ import databasePass from "./config.js"
 export const db = mysql.createConnection({
     host:"sql9.freesqldatabase.com",
     user:"sql9624101",
-    password: databasePass,
+    password: process.env.DB_PASS,
     database:"sql9624101"
 })
